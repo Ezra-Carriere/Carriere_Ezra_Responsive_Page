8ddda4122e6599f277a55ba661f9317c90bf3277
Ezra-Carriere <e_carriere150983@fanshaweonline.ca>
2023-11-12 20:12:44 -0500

    code updates

--- js/main.js
+++ js/main.js
@@ -1,46 +1,32 @@
 (() => {
-    const canvas = document.querySelector("#explode-view");
-    const context = canvas.getContext("2d");
-    canvas.width = 1920;
-    canvas.height = 1080;
-    const frameCount = 301; //how many still frames do we have?
-    const images = []; //an array to hold all of our images
-    //create an object literal with a property frame to hold the current frame
-    const buds = {
-        frame: 0
-    };
-
-    for (let i=0; i<frameCount; i++) {
-        //console.log(i);
-        //const img = new Image();
-        const img = document.createElement("img");
-        //need to recreate a string: images/explode_0001.webp
-        img.src = `images/earbud_img${(i+1).toString().padStart(4, '0')}.png`;
-        images.push(img);
+    //console.log("IIFE Fired");
+    //variables
+    const model = document.querySelector("#model");
+    const hotspots = document.querySelectorAll(".Hotspot");
+  
+    //functions
+    function modelLoaded() {
+      //console.log(hotspots);
+      hotspots.forEach(hotspot => {
+        hotspot.style.display = "block";
+      });
     }
-    //console.table(images)
-    
-    //Not actually animating a DOM element, but rather an object which contains a frame count
-    gsap.to(buds, {
-        frame: 300, 
-        snap: "frame",
-        scrollTrigger: {
-            trigger: "#explode-view",
-            pin: true,
-            scrub: 1,
-            start: "top top",
-            markers: true
-        },
-        onUpdate: render
-    })
-
-    images[0].addEventListener("onload", render);
-
-    function render() {
-        //console.log(buds.frame);
-        //console.log(images[buds.frame]);
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(images[buds.frame], 0, 0);
+  
+    function showInfo() {
+      let selected = document.querySelector(`button[slot="${this.slot}"] > div`);
+      gsap.to(selected, 1, { autoAlpha: 1 });
     }
-
-})();
+  
+    function hideInfo() {
+      let selected = document.querySelector(`button[slot="${this.slot}"] > div`);
+      gsap.to(selected, 1, { autoAlpha: 0 });
+    }
+  
+    //Event Listener
+    model.addEventListener("load", modelLoaded);
+  
+    hotspots.forEach(function (hotspot) {
+      hotspot.addEventListener("mouseover", showInfo);
+      hotspot.addEventListener("mouseout", hideInfo);
+    });
+  })();
